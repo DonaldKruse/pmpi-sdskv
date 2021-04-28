db75f61016a23b3b4b96ddb71afaff098484423f
Donald Kruse <krused@unm.edu>
2021-04-28 10:59:59 -0700

    removed old bin dir from run instructions

--- setup-db-and-run.sh
+++ setup-db-and-run.sh
@@ -4,6 +4,6 @@
 sdskv-server-daemon -f serveraddr.txt tcp pmpiDB &
 cat serveraddr.txt > input.txt
 cat otherargs.txt >> input.txt
-LD_PRELOAD=./bin/pmpi-lib.so lrun -N 2 -T 1 ./bin/main-generate $(<input.txt)
+LD_PRELOAD=./pmpi-lib.so lrun -N 2 -T 1 ./main-generate $(<input.txt)
 
-./bin/main-get $(<input.txt)
+./main-get $(<input.txt)
