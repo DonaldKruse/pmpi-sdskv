4d3a7e1a1fec44fbfd114425896d442324b4d388
Donald Kruse <krused@unm.edu>
2021-04-28 10:57:42 -0700

    Removing old file

--- setup-db-test-put-get.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#/g/g15/kruse11/Mochi/mochi-sdskv-fork/build/bin/sdskv-server-daemon -f serveraddr.txt tcp putGetTest &
-/g/g15/kruse11/Mochi/mochi-sdskv-fork/build/bin/sdskv-server-daemon -f serveraddr.txt tcp putGetTest &
-#sdskv-server-daemon -f serveraddr.txt tcp putGetTest &
-cat serveraddr.txt > input.txt
-cat otherargs.txt >> input.txt
-
